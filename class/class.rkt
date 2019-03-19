7d8e5bc38690ebb63870f4e59e3387c980ed557f
Dragan Ostojić <d.ostojic-ext@osram.com>
2019-03-19 14:03:05 -0400

    class

--- class/class.rkt
+++ class/class.rkt
@@ -4,14 +4,22 @@
 ;messages can be interpreted as method calls in OOP
 ;class in FP is a constructor which returns an object
 (define (Pair x y)
-  (lambda (msg)
+  (lambda (msg u)
     (cond
+      ; readers
       ((equal? msg "x?") x)
       ((equal? msg "y?") y)
+      ; setters
+      ((equal? msg "x=") (Pair u y))
+      ((equal? msg "y=") (Pair x u))
       (else
        "Error: Urecongnized message"))))
 
 (define pair1 (Pair 1 2))
-(pair1 "x?")
-(pair1 "y?")
-(pair1 "z?")
+(pair1 "x?" 0)
+(pair1 "y?" 0)
+(pair1 "z?" 0)
+(define pair3 (pair1 "x=" 3))
+(define pair4 (pair1 "y=" 4))
+(pair3 "x?" 0)
+(pair4 "y?" 0)
