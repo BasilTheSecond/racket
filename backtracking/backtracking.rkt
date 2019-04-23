27124060f565b036e9e5649bc450c4f870bbeab1
Dragan Ostojić <d.ostojic-ext@osram.com>
2019-04-23 11:07:00 -0400

    Saved

--- backtracking/backtracking.rkt
+++ backtracking/backtracking.rkt
@@ -1,1 +1,17 @@
 #lang racket
+
+; Position:
+(define-struct position (row column))
+
+; Occupancy is one of: 'occupied 'empty 'attacked
+; Position-state:
+(define-struct position-state (position occupancy))
+
+; Board is [List-of Position-state]
+
+(define (get-all-placements n)
+  empty)
+
+(get-all-placements 4)
+
+
