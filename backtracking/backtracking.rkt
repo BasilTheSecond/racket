a263705d4321cee560728500e7f70aa98a530860
Dragan Ostojić <d.ostojic-ext@osram.com>
2019-05-22 16:01:08 -0400

    Saved

--- backtracking/backtracking.rkt
+++ backtracking/backtracking.rkt
@@ -1,24 +1,30 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname backtracking) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
-;#lang racket
-
 ; Board is list of Rows
 ; Rows is a list of Columns
 ; Columns is list of Row Column Occupancy
 ; Row, Column is Int 1..n
 ; Occupancy is one of: 'occupied 'empty 'attacked
 
-
-(define (get-all-placements n)
-  empty)
-
-;(get-all-placements 4)
-
 (define (create-board n)
-  empty)
+  (local
+    ((define (create-rows row)
+       (if (> row n)
+           empty
+           (cons (create-columns 1 row) (create-rows (add1 row)))))
+     (define (create-columns column row)
+       (if (> column n)
+           empty
+           (cons (list row column 'empty) (create-columns (add1 column) row)))))
+    (create-rows 1)))
 
-(create-board 3)
+
+
+;(create-board 1)
+;(create-board 2)
+;(create-board 3)
+;(create-board 4)
 
 
 
